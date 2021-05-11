c10e45c96b092a3ef20ed74983a9bd65fc8f92f8
phamdinhkhanh <phamdinhkhanh.tkt53.neu@gmail.com>
2021-05-11 15:43:16 +0700

    add test.py

--- test.py
+++ test.py
@@ -1,12 +1,5 @@
 # Test data loader
 import vnquant.DataLoader as dl
-loader = dl.DataLoader('VND', '2018-02-02','2018-04-02')
-loader.download()
-
-# Test visualizatioin
-from vnquant import plot
-plot._vnquant_candle_stick(data='VND',
-                           title='VND stock price data and volume',
-                           xlab='Date', ylab='Price',
-                           start_date='2019-09-01', end_date='2019-11-01',
-                           show_vol=True)
+loader = dl.DataLoader('VND', '2018-02-02','2018-04-02', data_source='VND', minimal=True)
+data = loader.download()
+print(data.head())
